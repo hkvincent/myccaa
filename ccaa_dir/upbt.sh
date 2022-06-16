5a63cccda0f007d9a417846495787b9ac0c72589
vincentchan <hkvincent@users.noreply.github.com>
2022-06-16 09:54:01 +0800

    Update upbt.sh
    trackerslist in local

--- ccaa_dir/upbt.sh
+++ ccaa_dir/upbt.sh
@@ -9,6 +9,7 @@ export PATH
 
 function up_tracker(){
 	#下载最新的bt-tracker
+	#udp://tracker.opentrackr.org:1337/announce,udp://open.tracker.cl:1337/announce,udp://9.rarbg.com:2810/announce,udp://tracker.openbittorrent.com:6969/announce,udp://opentracker.i2p.rocks:6969/announce,https://opentracker.i2p.rocks:443/announce,http://tracker.openbittorrent.com:80/announce,udp://www.torrent.eu.org:451/announce,udp://tracker.torrent.eu.org:451/announce,udp://open.stealth.si:80/announce,udp://tracker.tiny-vps.com:6969/announce,udp://ipv4.tracker.harry.lu:80/announce,udp://exodus.desync.com:6969/announce,udp://tracker1.bt.moack.co.kr:80/announce,udp://tracker.zerobytes.xyz:1337/announce,udp://tracker.theoks.net:6969/announce,udp://tracker.publictracker.xyz:6969/announce,udp://tracker.pomf.se:80/announce,udp://tracker.monitorit4.me:6969/announce,udp://tracker.moeking.me:6969/announce
 	wget -O /tmp/trackers_best.txt https://api.xiaoz.org/trackerslist/
 	tracker=$(cat /tmp/trackers_best.txt)
 	#替换处理bt-tracker
